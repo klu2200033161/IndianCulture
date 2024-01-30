9325a6b607532d6d2cf9e4ca2ef885c8f501fc4d
Sailesh <2200033161@kluniversity.in>
2024-01-30 09:54:52 +0530

    New Homepage

--- DjangoProjects/ICMS/module/urls.py
+++ DjangoProjects/ICMS/module/urls.py
@@ -5,5 +5,7 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('login/', loginpage, name='loginpage'),
+    path('loginlog/', loginpagelog, name='loginpagelog'),
     path('register/', register, name='register'),
+    path('home1/',homepage1,name='homepage1')
 ]
