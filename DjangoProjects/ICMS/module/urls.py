d1e3f8724ec10b551a802ec8a7b135ef7dfa8380
Dhanush <satyavarapudhanush@gmail.com>
2024-01-31 17:03:49 +0530

    states modification

--- DjangoProjects/ICMS/module/urls.py
+++ DjangoProjects/ICMS/module/urls.py
@@ -7,22 +7,19 @@ urlpatterns = [
     path('login/', loginpage, name='loginpage'),
     path('register/', register, name='register'),
     path('home1/', homepage1, name='homepage1'),
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     path('cdance/',classicaldances,name='classicaldances'),
     path('fest/',festivals,name='festivals'),
     path('foods/',foods,name='foods'),
-=======
+
     path('states/', states, name='states'),
     path('historical/', historical, name='historical'),
->>>>>>> 02e84ad92093aa27f2f266ba7fea11a977ac55d1
-=======
+
     path('states/', states, name='states'),
     path('historical/', historical, name='historical'),
-=======
+
     path('cdance/',classicaldances,name='classicaldances'),
     path('fest/',festivals,name='festivals'),
     path('foods/',foods,name='foods'),
->>>>>>> 9c3e219a03b5ee89721679235af73b0188e599ca
->>>>>>> 396abfc6d5ca96b4a9de841f4d4039149f8f2ac3
+
 ]
