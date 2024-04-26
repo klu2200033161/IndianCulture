9c7f23a62e36efa525c2921a42fe8e1ce46da76f
Sailesh <2200033161@kluniversity.in>
2024-04-26 14:08:15 +0530

    hackathon1

--- DjangoProjects/ICMS/ICMS/settings.py
+++ DjangoProjects/ICMS/ICMS/settings.py
@@ -76,13 +76,13 @@ WSGI_APPLICATION = 'ICMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
  
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
-'''
+}'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -92,7 +92,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432'
     }
-}'''
+}
 
 
 # Password validation
--- DjangoProjects/ICMS/blogs/models.py
+++ DjangoProjects/ICMS/blogs/models.py
@@ -7,3 +7,10 @@ class Post(models.Model):
 
     def _str_(self):
         return self.title
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.title
