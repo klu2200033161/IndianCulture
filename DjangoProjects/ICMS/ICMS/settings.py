c613e917b593574ddb0bae1c72ad7893d391a00c
Sailesh <2200033161@kluniversity.in>
2024-04-26 23:23:14 +0530

    backend change for deployment

--- DjangoProjects/ICMS/ICMS/settings.py
+++ DjangoProjects/ICMS/ICMS/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'ICMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}'''
-DATABASES = {
+}
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'IndianCulture',
@@ -91,7 +91,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '5432'
     }
-}
+}'''
 
 
 # Password validation
